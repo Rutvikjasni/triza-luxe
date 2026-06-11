75cbe32f52c9193af237f79844ad41a302cd2d2a
rutvik_k_jasani <ritikjasani119@gmail.com>
2026-06-11 15:43:25 +0530

    Fix typo in next.config.ts image domain

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'qdtrakfzcjyjdttldd.supabase.co',
+        hostname: 'qdtrakfizcjkyjdttldd.supabase.co',
       },
     ],
   },
